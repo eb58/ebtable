87178634e69eabd2f20a1052009d3988dc1e3a3b
Eb <erich.brandl@googlemail.com>
2023-03-26 16:15:52 +0200

    mx refactor

--- public_html/javascript/ebtable/mx.js
+++ public_html/javascript/ebtable/mx.js
@@ -1,211 +1,141 @@
-/* global _ */
 //  2-dimensional array -- m(atri)x
-var mx = function (m, groupdef) {  //groupdef see below 
-  var basicapi = {
-    zero: function zero() {
-      return m.fill(0);
-    },
-    row: function row(n) {
-      return m[n];
-    },
-    rows: function rows(p) { // p = predicate-function or arr [1,4,5]
-      return m.filter(function (r, idx) {
-        return _.isFunction(p) ? p(m[r]) : _.indexOf(p, idx) >= 0;
-      });
-    },
-    withoutRows: function withoutRows(p) { // p = predicate-function or arr
-      return m.filter(function (r, idx) {
-        return _.isFunction(p) ? !p(r) : _.indexOf(p, idx) < 0;
-      });
-    },
-    col: function col(n) {
-      return _.range(m.length).map(function (r) {
-        return m[r][n];
-      });
-    },
-    cols: function (arr) {
-      var res = [];
-      for (var r = 0; r < m.length; r++) {
-        var row = m[r];
-        var nrow = [];
-        for (var c = 0; c < row.length; c++) {
-          if (_.indexOf(arr, c) >= 0)
-            nrow.push(row[c]);
-        }
-        res.push(nrow);
-      }
-      return res;
-    },
-    withoutCols: function (arr) {
-      var res = [];
-      for (var r = 0; r < m.length; r++) {
-        var row = m[r];
-        var nrow = [];
-        for (var c = 0; c < row.length; c++) {
-          if (_.indexOf(arr, c) < 0)
-            nrow.push(row[c]);
-        }
-        res.push(nrow);
-      }
-      return res;
+const mx = (m, groupDef) => {  //groupdef see below
+    const range = (n) => [...Array(n).keys()]
+    const basicApi = {
+        zero: () => m.fill(0),
+        row: (n) => m[n],
+        col: (n) => m.map((r) => r[n]),
+        rows: (p) => m.filter((r, idx) => !Array.isArray(p) ? p(m[r]) : p.indexOf(idx) >= 0), // p = predicate-function or arr  ~ [1,4,5]
     }
-  };
 
-//####################################  filtering #######################
-  var filtering = (function () {
-    var fcts = {
-      rowMatch: function rowMatch(filters) {
-        return function (row) {
-          filters = _.isArray(filters) ? filters : [filters];
-          var b = true;
-          for (var i = 0; i < filters.length && b; i++) {
-            var f = filters[i];
-            var cellData = $.trim(row[f.col]);
-            var matchfct = f.match || $.fn.ebtable.matcher['starts-with-matches'];
-            b = b && matchfct(cellData, f.searchtext, row, m);
-          }
-          return b;
+    //####################################  filtering #######################
+    const filtering = (() => {
+        const fcts = { // filters [{col, searchText, renderer}, ...]
+            rowMatch: (filters) => (row) => (Array.isArray(filters) ? filters : [filters]).reduce((acc, f) => {
+                const cellData = $.trim(row[f.col]);
+                const matchFct = f.match || $.fn.ebtable.matcher['starts-with-matches'];
+                return acc && matchFct(cellData, f.searchtext, row, m);
+            }, true),
+            filterData: (filters) => m.filter(fcts.rowMatch(filters))
         };
-      },
-      filterData: function filterData(filters) { // filters [{col: col, searchtext: text, render:myrenderer},...]
-        return this.filter(fcts.rowMatch(filters));
-      }
-    };
-    return {
-      filterData: fcts.filterData
-    };
-  })();
-
-//####################################  grouping #######################
-  var grouping = (function () {
-    // // groupdefs  ~ {grouplabel: 0, groupcnt: 1, groupid: 2, groupsortstring: 3, groupname: 4, grouphead: 'GA', groupelem: 'GB'}
-    var fcts = {
-      normalizeGroupId: function (id) {
-        return id <= 0 ? 0 : id;
-      },
-      isGroupingHeader: function isGroupingHeader(row, groupdefs) {
-        return row[groupdefs.grouplabel] === groupdefs.grouphead;
-      },
-      initGroups: function initGroups(groupdefs) {
-        if (!groupdefs.groupid)
-          return;
-        var row, groupId;
-        var groupsdata = this.groupsdata || {};
-        for (var r = 0; r < this.length; r++) {
-          row = this[r];
-          groupId = fcts.normalizeGroupId(row[groupdefs.groupid]);
-          row.isGroupHeader = row[groupdefs.grouplabel] === groupdefs.grouphead;
-          row.isGroupElement = groupId && !row.isGroupHeader;
-          if (groupId && !groupsdata[groupId]) {
-            groupsdata[groupId] = {isOpen: false, groupname: $.trim(row[groupdefs.groupname])};
-          }
-        }
-        for (r = 0; r < this.length; r++) {
-          row = this[r];
-          groupId = fcts.normalizeGroupId(row[groupdefs.groupid]);
-          row[groupdefs.groupsortstring] = groupId ? (groupsdata[groupId].groupname + ' ' + groupId) : row[groupdefs.groupname];
-        }
-        this.groupsdata = groupsdata;
-        return this;
-      },
-      filterGroups: function filterGroups(groupdefs, groupsdata) {
-        var filteredData = this.filter( function (row) {
-          var groupId = fcts.normalizeGroupId((row[groupdefs.groupid]));
-          return(!groupId || fcts.isGroupingHeader(row, groupdefs) || groupsdata[groupId].isOpen);
-        });
-        filteredData.groupsdata = this.groupsdata;
-        return filteredData;
-      },
-      getGroupRows: function getGroupRows(groupdefs, groupid) {
-        return this.filter( function (row) {
-          return row[groupdefs.groupid] === groupid;
-        });
-      }
-    };
-    return {
-      initGroups: fcts.initGroups,
-      filterGroups: fcts.filterGroups,
-      getGroupRows: fcts.getGroupRows
-    };
-  })();
-
-  //####################################  sorting #######################
-  var sorting = (function () {
-    var fcts = {
-      toLower: function toLower(o) {
-        return _.isString(o) ? o.toLowerCase() : o;
-      },
-      prepareItem: function prepareItem(row, col, fmt, groups, sortorder) {
-        var v = row[col] || '';
-        return fcts.toLower(fmt ? fmt(v, row, groups, sortorder) : v);
-      },
-      rowCmpCols: function rowCmpCols(coldefs, groups) {
-        coldefs = _.isArray(coldefs) ? coldefs : [coldefs]; // [ {col:1,sortorder:asc,sortformat:fmtfct1},{col:3, sortorder:desc, sortformat:fmtfct2},... ]  
-        return function (r1, r2) {
-          for (var i = 0; i < coldefs.length; i++) {
-            var cdef = coldefs[i];
-            var fmt = cdef.sortformat ? $.fn.ebtable.sortformats[cdef.sortformat] : undefined;
-            var x = fcts.prepareItem(r1, cdef.col, fmt, groups, cdef.sortorder);
-            var y = fcts.prepareItem(r2, cdef.col, fmt, groups, cdef.sortorder);
-            var ret = 0;
-            if( typeof x === 'string' && typeof y === 'string' ){
-              ret = x.localeCompare(y);
-            }
-            else{
-              ret = (x < y) ? -1 : ((x > y) ? 1 : 0);
-            }
-            if (ret !== 0) {
-              var bAsc = !cdef.sortorder || cdef.sortorder.indexOf('desc') < 0;
-              return bAsc ? ret : -ret;
-            }
-          }
-          return 0;
+        return {
+            filterData: fcts.filterData
         };
-      }
-    };
-    return {
-      rowCmpCols: fcts.rowCmpCols
-    };
-  })();
-  //####################################  pageing #######################
-  var pageing = (function () {
-    var page = 0;
-    var pageSize = 10; // fcts.setPageSize(10);
-    var pageMax = Math.floor((this.length - 1) / pageSize);
+    })();
 
-    var fcts = {
-      setPageSize: function (n) {
-        page = 0;
-        pageSize = n;
-        pageMax = Math.floor((this.length - 1) / n);
-      },
-      getCurPageData: function () {
-        var startRow = pageSize * page;
-        return this.rows(_.range(startRow, startRow + pageSize));
-      }
-    };
-    return {
-      pageFirst: function pageFirst() {
-        page = 0;
-      },
-      pagePrev: function pagePrev() {
-        page = Math.max(0, page - 1);
-      },
-      pageNext: function pageNext() {
-        page = Math.min(page + 1, pageMax);
-      },
-      pageLast: function pageLast() {
-        page = pageMax;
-      },
-      setPageSize: fcts.setPageSize,
-      getCurPageData: fcts.getCurPageData
-    };
-  })();
-  //#####################################################################
+    //####################################  grouping #######################
+    const grouping = (() => {
+        // // groupDefs  ~ {grouplabel: 0, groupcnt: 1, groupid: 2, groupsortstring: 3, groupname: 4, grouphead: 'GA', groupelem: 'GB'}
+        const fcts = {
+            normalizeGroupId: (id) => id <= 0 ? 0 : id,
+            isGroupingHeader: (row, groupDefs) => row[groupDefs.grouplabel] === groupDefs.grouphead,
+            initGroups: (groupDefs) => {
+                if (!groupDefs.groupid)
+                    return;
+                let row, groupId;
+                const groupsData = m.groupsData || {};
+                for (let r = 0; r < m.length; r++) {
+                    row = m[r];
+                    groupId = fcts.normalizeGroupId(row[groupDefs.groupid]);
+                    row.isGroupHeader = row[groupDefs.grouplabel] === groupDefs.grouphead;
+                    row.isGroupElement = groupId && !row.isGroupHeader;
+                    if (groupId && !groupsData[groupId]) {
+                        groupsData[groupId] = {isOpen: false, groupname: $.trim(row[groupDefs.groupname])};
+                    }
+                }
+                for (let r = 0; r < m.length; r++) {
+                    row = m[r];
+                    groupId = fcts.normalizeGroupId(row[groupDefs.groupid]);
+                    row[groupDefs.groupsortstring] = groupId ? (groupsData[groupId].groupname + ' ' + groupId) : row[groupDefs.groupname];
+                }
+                m.groupsdata = groupsData;
+                return m;
+            },
+            filterGroups: (groupDefs, groupsdata) => {
+                const filteredData = m.filter((row) => {
+                    const groupId = fcts.normalizeGroupId((row[groupDefs.groupid]));
+                    return (!groupId || fcts.isGroupingHeader(row, groupDefs) || groupsdata[groupId].isOpen);
+                });
+                filteredData.groupsdata = m.groupsdata;
+                return filteredData;
+            },
+            getGroupRows: (groupDefs, groupid) => m.filter((row) => row[groupDefs.groupid] === groupid)
 
-  var res = _.extend(m, basicapi, sorting, filtering, grouping);
-  if (groupdef) {
-    res.initGroups(groupdef);
-  }
-  return res;
+        };
+        return {
+            initGroups: fcts.initGroups,
+            filterGroups: fcts.filterGroups,
+            getGroupRows: fcts.getGroupRows
+        };
+    })();
+    //####################################  sorting #######################
+    const sorting = (() => {
+        const fcts = {
+
+            rowCmpCols: (colDefs, groups) => {
+                const toLower = (o) => typeof o === 'string' ? o.toLowerCase() : o;
+                const prepareItem = (row, col, fmt, groups, sortorder) => {
+                    const v = row[col] || '';
+                    return toLower(fmt ? fmt(v, row, groups, sortorder) : v)
+                };
+                colDefs = Array.isArray(colDefs) ? colDefs : [colDefs]; // [ {col:1,sortorder:asc,sortformat:fmtfct1},{col:3, sortorder:desc, sortformat:fmtfct2},... ]
+                return (r1, r2) => {
+                    for (let i = 0; i < colDefs.length; i++) {
+                        const cdef = colDefs[i];
+                        const fmt = cdef.sortformat ? $.fn.ebtable.sortformats[cdef.sortformat] : undefined;
+                        const x = prepareItem(r1, cdef.col, fmt, groups, cdef.sortorder);
+                        const y = prepareItem(r2, cdef.col, fmt, groups, cdef.sortorder);
+                        let ret = 0;
+                        if (typeof x === 'string' && typeof y === 'string') {
+                            ret = x.localeCompare(y);
+                        } else {
+                            ret = (x < y) ? -1 : ((x > y) ? 1 : 0);
+                        }
+                        if (ret !== 0) {
+                            const bAsc = !cdef.sortorder || cdef.sortorder.indexOf('desc') < 0;
+                            return bAsc ? ret : -ret;
+                        }
+                    }
+                    return 0;
+                };
+            }
+        };
+        return {
+            rowCmpCols: fcts.rowCmpCols
+        };
+    })();
+    //####################################  paging #######################
+    const paging = (() => {
+        let page = 0;
+        let pageSize = 10; // fcts.setPageSize(10);
+        let pageMax = Math.floor((m.length - 1) / pageSize);
+
+        const fcts = {
+            setPageSize: (n) => {
+                page = 0;
+                pageSize = n;
+                pageMax = Math.floor((m.length - 1) / n);
+            },
+            getCurPageData: () => {
+                const startRow = pageSize * page;
+                return m.rows(range(startRow, startRow + pageSize));
+            }
+        };
+        return {
+            pageFirst: () => page = 0,
+            pagePrev: () => page = Math.max(0, page - 1),
+            pageNext: () => page = Math.min(page + 1, pageMax),
+            pageLast: () => page = pageMax,
+            setPageSize: fcts.setPageSize,
+            getCurPageData: fcts.getCurPageData
+        };
+    })();
+    //#####################################################################
+
+    const res = _.extend(m, basicApi, sorting, filtering, grouping, paging);
+
+    if (groupDef) {
+        res.initGroups(groupDef);
+    }
+    return res;
 };
--- public_html/tests/unittests/mx/mxtest2.js
+++ public_html/tests/unittests/mx/mxtest2.js
@@ -1,6 +1,6 @@
 /* global QUnit */
 
-var tblData = [
+const tblData = [
   ['  ', 1, -1, 'Wolperdinger', '31.08.1915', 'Herder, Johann Gottfried', 5],
   ['  ', 1, -1, 'Yeti        ', '31.08.2016', 'Lichtenberg, Georg Christoph ', 31],
   ['GA', 5, 10, 'Säugetiere  ', '', '', 0],
@@ -20,9 +20,9 @@ tblData.forEach(function (row) {
 
 
 QUnit.test('testset: grouping functions', function (assert) {
-  var m = mx(tblData.slice());
+  const m = mx(tblData.slice());
 
-  var groupingCols = {grouplabel: 0, groupcnt: 1, groupid: 2, groupsortstring: 3, groupname: 4, grouphead: 'GA', groupelem: 'GB'};
+  const groupingCols = {grouplabel: 0, groupcnt: 1, groupid: 2, groupsortstring: 3, groupname: 4, grouphead: 'GA', groupelem: 'GB'};
   m.initGroups(groupingCols);
   assert.deepEqual(m.groupsdata, {
     "10": {isOpen: false, groupname: "Säugetiere"},
@@ -30,7 +30,7 @@ QUnit.test('testset: grouping functions', function (assert) {
     "30": {isOpen: false, groupname: "Fische"
     }
   });
-  var fg = m.filterGroups(groupingCols, m.groupsdata);
+  let fg = m.filterGroups(groupingCols, m.groupsdata);
   assert.deepEqual(fg, [
     ["  ", 1, -1, "Wolperdinger", "Wolperdinger", "31.08.1915", "Herder, Johann Gottfried", 5],
     ["  ", 1, -1, "Yeti        ", "Yeti        ", "31.08.2016", "Lichtenberg, Georg Christoph ", 31],
@@ -40,7 +40,7 @@ QUnit.test('testset: grouping functions', function (assert) {
     ["  ", 1, -1, "Einhorn     ", "Einhorn     ", "04.11.2017", "Goethe, Johann Wolfgang", 63]
   ]);
   m.groupsdata[20].isOpen = true;
-  var fg = m.filterGroups(groupingCols, m.groupsdata);
+  fg = m.filterGroups(groupingCols, m.groupsdata);
   assert.deepEqual(fg, [
     ["  ", 1, -1, "Wolperdinger", "Wolperdinger", "31.08.1915", "Herder, Johann Gottfried", 5],
     ["  ", 1, -1, "Yeti        ", "Yeti        ", "31.08.2016", "Lichtenberg, Georg Christoph ", 31],
@@ -65,8 +65,8 @@ QUnit.test('testset: grouping functions', function (assert) {
 });
 
 QUnit.test('testset: sorting functions', function (assert) {
-  var m = mx(tblData.slice());
-  var groupingCols = {grouplabel: 0, groupcnt: 1, groupid: 2, groupsortstring: 3, groupname: 4, grouphead: 'GA', groupelem: 'GB'};
+  const m = mx(tblData.slice());
+  const groupingCols = {grouplabel: 0, groupcnt: 1, groupid: 2, groupsortstring: 3, groupname: 4, grouphead: 'GA', groupelem: 'GB'};
   m.initGroups(groupingCols);
   assert.deepEqual(m.sort(m.rowCmpCols([{col: 2, sortorder: 'asc'}, {col: 0, sortorder: 'asc-fix'}, {col: 5, sortorder: 'asc', sortformat: 'date-de'}])), [
     ['  ', 1, -1, 'Wolperdinger', "Wolperdinger", '31.08.1915', 'Herder, Johann Gottfried', 5],
