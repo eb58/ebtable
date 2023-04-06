3c3d9f1615eef08e09b475c2484c84487d62b27f
Erich Brandl <erich.brandl@atos.net>
2023-04-06 15:01:42 +0200

    refactor ebtable to use modern javascript

--- public_html/tests/unittests/ebtable/testHelpers.js
+++ public_html/tests/unittests/ebtable/testHelpers.js
@@ -1,15 +1,13 @@
 /* global _ */
 
-function clearLocalStorage() {
-  Object.keys(localStorage).forEach((o) => o.startsWith('unittest') && delete localStorage[o]);
-}
+const clearLocalStorage = () => Object.keys(localStorage).forEach((o) => o.includes('unittest') && delete localStorage[o]);
 
 const getCol = (gridname, n) =>
   $('#' + gridname + ' table tbody td:nth-child(' + n + ')')
     .toArray()
     .reduce((acc, o) => [...acc, $(o).text().trim()], []);
 
-function reinit(gridname, opts, data) {
+const reinit = (gridname, opts, data) => {
   clearLocalStorage();
   $('#' + gridname).ebtable(opts, data);
-}
+};
