7dbc57c90e5a3ede8932c450c433f77b327c0386
Erich Brandl <erich.brandl@atos.net>
2023-04-17 13:34:41 +0200

    add jquery plugin ebpager.js

--- public_html/javascript/ebtable/ebpager.js
+++ public_html/javascript/ebtable/ebpager.js
@@ -1,11 +1,11 @@
-$.fn.pager = function (opts, callback) {
+$.fn.ebpager = function (opts, callback) {
   const template = (str) => (opts) => Object.keys(opts).reduce((acc, key) => acc.replaceAll(`<%=${key}%>`, opts[key] || ''), str.replace(/<%= */g, '<%=').replace(/ *%>/g, '%>'));
-  const translate = (str) => $.fn.pager.lang[myOpts.lang || 'de'][str] || str;
+  const translate = (str) => $.fn.ebpager.lang[myOpts.lang || 'de'][str] || str;
 
   if (opts.lengthOfData === undefined) throw Error('length option must be set!');
 
   const defOpts = {
-    gridId: '',
+    pagerId: '',
     withPagelenctrl: true,
     rowsPerPageSelectValues: [5, 10, 25, 50, 100],
     rowsPerPage: 5
@@ -34,19 +34,19 @@ $.fn.pager = function (opts, callback) {
   };
 
   this.html(`
-    <span id='eb-pager'>
-      <span id='ctrlInfo'></span>
+    <span class='eb-pager'>
+      <span class='ctrlInfo'></span>
       <button class='firstBtn'>&#8676;</button>
-      <button class='backBtn'> &larr; </button>
-      <span id='ctrlSelect'></span>
+      <button class='prevBtn'> &larr; </button>
+      <span class='ctrlSelect'></span>
       <button class='nextBtn'> &rarr; </button>
       <button class='lastBtn'> &#8677;</button>
     </span>`);
-  $(myOpts.gridId + '#ctrlInfo').text(ctrlInfo());
-  $(myOpts.gridId + '#ctrlSelect').html(selectLenCtrl());
+  $(myOpts.pagerId + '.ctrlInfo').text(ctrlInfo());
+  $(myOpts.pagerId + '.ctrlSelect').html(selectLenCtrl());
 
   // On change page length
-  $(myOpts.gridId + '#pageLenCtrl').on('change', (ev) => {
+  $(myOpts.pagerId + '#pageLenCtrl').on('change', (ev) => {
     myOpts.rowsPerPage = Number($(ev.target).val());
     currentPage = 0;
     currentPageMax = Math.floor(Math.max(0, myOpts.lengthOfData - 1) / myOpts.rowsPerPage);
@@ -56,22 +56,23 @@ $.fn.pager = function (opts, callback) {
 
   const setCurrentPage = (n) => {
     currentPage = Math.min(Math.max(0, n), currentPageMax);
-    $(myOpts.gridId + '#ctrlInfo').text(ctrlInfo());
+    $(myOpts.pagerId + '.ctrlInfo').text(ctrlInfo());
     callback && callback(currentPage);
   };
   const addHandler = (selector, action) =>
-    $(myOpts.gridId + selector)
+    $(myOpts.pagerId + selector)
       .off()
       .on('click', action);
 
-  addHandler('.firstBtn', () => setCurrentPage(0));
-  addHandler('.backBtn', () => setCurrentPage(currentPage - 1));
-  addHandler('.nextBtn', () => setCurrentPage(currentPage + 1));
-  addHandler('.lastBtn', () => setCurrentPage(currentPageMax));
+  addHandler(myOpts.pagerId + '.firstBtn', () => setCurrentPage(0));
+  addHandler(myOpts.pagerId + '.prevBtn', () => setCurrentPage(currentPage - 1));
+  addHandler(myOpts.pagerId + '.nextBtn', () => setCurrentPage(currentPage + 1));
+  addHandler(myOpts.pagerId + '.lastBtn', () => setCurrentPage(currentPageMax));
+
   return this;
 };
 
-$.fn.pager.lang = {
+$.fn.ebpager.lang = {
   de: {},
   en: {
     '<%=start%>-<%=end%> von <%=count%> Zeilen': '<%=start%> to <%=end%> of <%=count%> rows'
