7bcc62eb309c1226c421265c7a7388a07924433e
Erich Brandl <erich.brandl@atos.net>
2023-04-17 16:11:55 +0200

    disable browse buttons

--- public_html/javascript/ebtable/ebpager.js
+++ public_html/javascript/ebtable/ebpager.js
@@ -1,11 +1,12 @@
 $.fn.ebpager = function (opts, callback) {
+  if (!opts.pagerId) throw Error('pagerId option must be set!');
+  if (opts.lengthOfData === undefined) throw Error('length option must be set!');
+  if (opts.rowsPerPage !== opts.rowsPerPageSelectValues[0]) throw Error('rowsPerPage must be equal to first in rowsPerPageSelectValues!');
+
   const template = (str) => (opts) => Object.keys(opts).reduce((acc, key) => acc.replaceAll(`<%=${key}%>`, opts[key] || ''), str.replace(/<%= */g, '<%=').replace(/ *%>/g, '%>'));
   const translate = (str) => $.fn.ebpager.lang[myOpts.lang || 'de'][str] || str;
 
-  if (opts.lengthOfData === undefined) throw Error('length option must be set!');
-
   const defOpts = {
-    pagerId: '',
     withPagelenctrl: true,
     rowsPerPageSelectValues: [5, 10, 25, 50, 100],
     rowsPerPage: 5
@@ -24,29 +25,27 @@ $.fn.ebpager = function (opts, callback) {
   };
 
   const ctrlInfo = () => {
-    const startRow = Math.min(myOpts.rowsPerPage * currentPage + 1, myOpts.lengthOfData);
-    const endRow = Math.min(startRow + myOpts.rowsPerPage - 1, myOpts.lengthOfData);
+    const start = Math.min(myOpts.rowsPerPage * currentPage + 1, myOpts.lengthOfData);
+    const end = Math.min(start + myOpts.rowsPerPage - 1, myOpts.lengthOfData);
     return template(translate('<%=start%>-<%=end%> von <%=count%> Zeilen'))({
-      start: startRow,
-      end: endRow,
+      start,
+      end,
       count: myOpts.lengthOfData
     });
   };
 
   this.html(`
-    <span class='eb-pager'>
-      <span class='ctrlInfo'></span>
+    <span id=${opts.pagerId} class='eb-pager'>
       <button class='firstBtn'>&#8676;</button>
-      <button class='prevBtn'> &larr; </button>
-      <span class='ctrlSelect'></span>
-      <button class='nextBtn'> &rarr; </button>
-      <button class='lastBtn'> &#8677;</button>
+      <button class='prevBtn'>&larr;</button>
+      <span class='ctrlSelect'>${selectLenCtrl()}</span>
+      <button class='nextBtn'>&rarr;</button>
+      <button class='lastBtn'>&#8677;</button>
+      <span class='ctrlInfo'>${ctrlInfo()}</span>
     </span>`);
-  $(myOpts.pagerId + '.ctrlInfo').text(ctrlInfo());
-  $(myOpts.pagerId + '.ctrlSelect').html(selectLenCtrl());
 
   // On change page length
-  $(myOpts.pagerId + '#pageLenCtrl').on('change', (ev) => {
+  $('#' + myOpts.pagerId + ' #pageLenCtrl').on('change', (ev) => {
     myOpts.rowsPerPage = Number($(ev.target).val());
     currentPage = 0;
     currentPageMax = Math.floor(Math.max(0, myOpts.lengthOfData - 1) / myOpts.rowsPerPage);
@@ -56,18 +55,22 @@ $.fn.ebpager = function (opts, callback) {
 
   const setCurrentPage = (n) => {
     currentPage = Math.min(Math.max(0, n), currentPageMax);
-    $(myOpts.pagerId + '.ctrlInfo').text(ctrlInfo());
+    $('#' + myOpts.pagerId + ' .ctrlInfo').text(ctrlInfo());
+    $('#' + myOpts.pagerId + ' .firstBtn').prop('disabled', currentPage === 0);
+    $('#' + myOpts.pagerId + ' .prevBtn').prop('disabled', currentPage === 0);
+    $('#' + myOpts.pagerId + ' .nextBtn').prop('disabled', currentPage === currentPageMax);
+    $('#' + myOpts.pagerId + ' .lastBtn').prop('disabled', currentPage === currentPageMax);
     callback && callback(currentPage);
   };
   const addHandler = (selector, action) =>
-    $(myOpts.pagerId + selector)
+    $('#' + myOpts.pagerId + ' ' + selector)
       .off()
       .on('click', action);
 
-  addHandler(myOpts.pagerId + '.firstBtn', () => setCurrentPage(0));
-  addHandler(myOpts.pagerId + '.prevBtn', () => setCurrentPage(currentPage - 1));
-  addHandler(myOpts.pagerId + '.nextBtn', () => setCurrentPage(currentPage + 1));
-  addHandler(myOpts.pagerId + '.lastBtn', () => setCurrentPage(currentPageMax));
+  addHandler('.firstBtn', () => setCurrentPage(0));
+  addHandler('.prevBtn', () => setCurrentPage(currentPage - 1));
+  addHandler('.nextBtn', () => setCurrentPage(currentPage + 1));
+  addHandler('.lastBtn', () => setCurrentPage(currentPageMax));
 
   return this;
 };
